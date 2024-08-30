5b4ba53f48e719116f844a28833b4e117a2e8476
mobaijun <mobaijun8@163.com>
2024-08-30 15:22:23 +0800

    :art: update

--- ballcat-codegen-backend/src/main/resources/docker/build.sh
+++ ballcat-codegen-backend/src/main/resources/docker/build.sh
@@ -4,6 +4,7 @@
 DOCKER_USERNAME="mobaijun"
 IMAGE_NAME="generator"
 TAG="latest"
+EXPORT_NAME="${IMAGE_NAME}_${TAG}"
 
 # 构建 Docker 镜像
 echo "Building Docker image..."
@@ -15,9 +16,25 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# 查看构建的镜像
-echo "Docker images:"
-docker images | grep $IMAGE_NAME
+# 导出并压缩镜像为 tar.gz 文件
+echo "Exporting and compressing Docker image..."
+docker save $DOCKER_USERNAME/$IMAGE_NAME:$TAG | gzip > ${EXPORT_NAME}.tar.gz
+
+# 检查导出和压缩是否成功
+if [ $? -ne 0 ]; then
+    echo "Export or compression failed. Exiting."
+    exit 1
+fi
+
+# 从压缩文件中加载 Docker 镜像
+echo "Loading Docker image from compressed file..."
+gunzip -c ${EXPORT_NAME}.tar.gz | docker load
+
+# 检查加载是否成功
+if [ $? -ne 0 ]; then
+    echo "Docker load failed. Exiting."
+    exit 1
+fi
 
 # 推送镜像到 Docker Hub
 echo "Pushing Docker image to Docker Hub..."
@@ -29,4 +46,4 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Docker image successfully pushed to Docker Hub."
+echo "Docker image successfully built, compressed, loaded, and pushed to Docker Hub."
